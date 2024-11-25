c5cd196a5103daaaacd20c3e7812dccbacaef3a3
Sergio González Sánchez <soysergiogonza@gmail.com>
2024-11-25 17:01:06 -0500

    add: tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url('/assets/images/hero-banner.png')",
+        "dots-pattern": "url('/assets/images/dots.png')",
+      },
+      colors: {
+        majica: {
+          primary: "rgba(240, 134, 118, 1)", // Color oscuro del logo
+          secondary: "#38B6B0", // Color turquesa/aqua del logo
+          coral: "#E8927C", // Color coral/salmón de los elementos decorativos
+          "coral-light": "#FFF5F3", // Fondo coral claro
+          text: {
+            primary: "#28283D",
+            secondary: "#2BB4AC", // Color texto secundario
+            light: "#667085", // Color texto gris
+          },
+          landing: {
+            hero: "url('/images/hero.jpg')",
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
