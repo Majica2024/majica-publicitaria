1b28fa8198bcf5d309d3a681c3c22de6e7c7fef3
Sergio González Sánchez <soysergiogonza@gmail.com>
2024-12-03 15:42:14 -0500

    change: se cambia la altura del botón de wp

--- src/components/ui/WhatsAppButton/index.tsx
+++ src/components/ui/WhatsAppButton/index.tsx
@@ -7,7 +7,7 @@ export const WhatsAppButton = () => {
     <Link
       href={`https://wa.me/${NEXT_PUBLIC_WHATSAPP_NUMBER}`}
       target='_blank'
-      className='fixed bottom-12 right-8 z-50 flex items-center justify-center w-14 h-14 bg-[#25D366] rounded-full shadow-lg hover:scale-110 transition-transform duration-300 hover:shadow-xl'
+      className='fixed bottom-12 md:bottom-8 right-8 z-50 flex items-center justify-center w-14 h-14 bg-[#25D366] rounded-full shadow-lg hover:scale-110 transition-transform duration-300 hover:shadow-xl'
       aria-label='Contactar por WhatsApp'
     >
       <IoLogoWhatsapp size={32} className='text-white' />
